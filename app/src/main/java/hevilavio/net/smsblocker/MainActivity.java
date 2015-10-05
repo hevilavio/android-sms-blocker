d4c1dbcedaaf5a498071600c180b1b584d3627d0
hevilavio <bld_hsoares@uolinc.com>
2015-10-05 14:13:02 -0300

    Refactor and removal of unused code

--- app/src/main/java/hevilavio/net/smsblocker/MainActivity.java
+++ app/src/main/java/hevilavio/net/smsblocker/MainActivity.java
@@ -28,30 +28,8 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /** Called when the user clicks the Confirm button */
-    public void confirmName(View view) {
+    public void sendName(View view) {
         Intent displayFirstNameActivity = new Intent(this, RegisteredUserActivity.class);
         EditText firstName = (EditText) findViewById(R.id.first_name);
         String firstNameContent = firstName.getText().toString();
